53a584cd5df4c6dda6c9ff6b53b1a81e11143b0b
Like Zhang <likezhang@Likes-MacBook-Air.local>
2023-08-09 13:53:22 -0700

    update with argparser

--- server.py
+++ server.py
@@ -2,21 +2,53 @@ from aiohttp import web
 import torch
 import base64, os
 from io import BytesIO
+import argparse
 
 from diffusers import StableDiffusionPipeline
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
-pipe.to("cuda")
-
+pipeline = None
 SERVER_TOKEN = os.environ.get("SERVER_TOKEN", "123456")
 
-def txt2img(prompt):
-    img = pipe(prompt, num_inference_steps=10).images[0]
-    buffered = BytesIO()
-    img.save(buffered, format="JPEG")
-    base64_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+
+def initConfig():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, required=True, help='base model path or name on huggingface')
+    parser.add_argument('--lora', type=str, default='', help='lora location')
+    parser.add_argument('--token', type=str, default="123456", help='server token')
+    # Parse the arguments
+    args = parser.parse_args()
+    return args
+
+
+def txt2img(prompts):
+    images = pipeline(prompts, num_inference_steps=10).images
+    result = []
+    for img in images:
+        buffered = BytesIO()
+        img.save(buffered, format="JPEG")
+        base64_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+        result.append({'base64_str': base64_str})
     # print(base64_str)
-    return base64_str
+    return result
+
+
+def init():
+    conf = initConfig()
+    if conf.token != '':
+        SERVER_TOKEN = conf.token
+
+    model = conf.model
+    loraPath = conf.lora    
+    # pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
+    if model.endswith('.safetensors') or model.endswith('.ckpt'):
+        pipeline = StableDiffusionPipeline.from_single_file(model)
+    else:
+        pipeline = StableDiffusionPipeline.from_pretrained(model, revision="fp16", torch_dtype=torch.float16)
+    
+    if conf.lora != '':
+        pipeline.unet.load_attn_procs(conf.lora)
+
+    pipeline.to("cuda")
 
 
 routes = web.RouteTableDef()
@@ -37,9 +69,11 @@ async def text_to_image_handle(request):
     data = txt2img(prompt)
     return web.json_response({'result': data})
 
-
 app = web.Application()
 app.add_routes(routes)
 
 if __name__ == '__main__':
+    init()
+
+
     web.run_app(app, port=8085)
