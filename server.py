d55253dcea01ff67aad23a75885071ef90b2c8bd
Like Zhang <likezhang@Likes-MacBook-Air.local>
2023-08-09 17:09:16 -0700

    set width

--- server.py
+++ server.py
@@ -17,9 +17,11 @@ pipeline = None
 SERVER_TOKEN = os.environ.get("SERVER_TOKEN", "123456")
 NUM_OF_IMAGES = 4
 STEPS = 100
-HEIGHT=256
-WIDTH=256
+HEIGHT=480
+WIDTH=480
 STEPS = 50
+
+NEGATIVE_PROMPT="(worst quality, low quality, normal quality:1.4), lowres, bad anatomy, ((bad hands)), text, error, missing fingers, extra digit, fewer digits,head out of frame, cropped, letterboxed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, censored, letterbox, blurry, monochrome, fused clothes, nail polish, boring, extra legs, fused legs, missing legs, missing arms, extra arms, fused arms, missing limbs, mutated limbs, dead eyes, empty eyes, 2girls, multiple girls, 1boy, 2boys, multiple boys, multiple views, jpeg artifacts, text, signature, watermark, artist name, logo, low res background, low quality background, missing background, white background,deformed"
 
 def initConfig():
     parser = argparse.ArgumentParser()
@@ -27,17 +29,20 @@ def initConfig():
     parser.add_argument('--lora', type=str, default='', help='lora location')
     parser.add_argument('--token', type=str, default="123456", help='server token')
     parser.add_argument('--steps', type=int, default=50, help='steps')
+    parser.add_argument('--width', type=int, default=480, help='image width (same height)')
     # Parse the arguments
     args = parser.parse_args()
     return args
 
 
+
 def txt2img(prompt):
     global pipeline
-    images = pipeline(prompt, 
-                      num_images_per_prompt=NUM_OF_IMAGES, 
-                      num_inference_steps=STEPS, 
-                      height=HEIGHT, 
+    images = pipeline(prompt,
+                      negative_prompt=NEGATIVE_PROMPT,
+                      num_images_per_prompt=NUM_OF_IMAGES,
+                      num_inference_steps=STEPS,
+                      height=HEIGHT,
                       width=WIDTH).images
     result = []
     for img in images:
@@ -50,24 +55,27 @@ def txt2img(prompt):
 
 
 def init():
-    global pipeline, SERVER_TOKEN, STEPS
+    global pipeline, SERVER_TOKEN, STEPS, WIDTH, HEIGHT
     conf = initConfig()
     if conf.token != '':
         SERVER_TOKEN = conf.token
-    
+
     STEPS = conf.steps
-    
+    WIDTH = conf.width
+    HEIGHT = WIDTH
+
     model = conf.model
-    loraPath = conf.lora    
+    loraPath = conf.lora
     print("model: {}\nlora:{}".format(model, loraPath))
     # pipeline = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16)
     if model.endswith('.safetensors') or model.endswith('.ckpt'):
         pipeline = StableDiffusionPipeline.from_single_file(model, safety_checker = None, requires_safety_checker = False)
     else:
         pipeline = StableDiffusionPipeline.from_pretrained(model, revision="fp16", torch_dtype=torch.float16, safety_checker = None, requires_safety_checker = False)
-    
+
     if loraPath != '':
         if loraPath.endswith('.safetensors'):
+            print("load lora weights")
             pipeline.load_lora_weights(".", weight_name=loraPath)
         else:
             pipeline.unet.load_attn_procs(conf.lora)
