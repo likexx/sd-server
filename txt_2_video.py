e6bf8707dfb5baa3b8d541fd72bd0bcb86a2404a
likezhang <likezhang@Likes-MacBook-Air.local>
2024-02-04 15:17:16 -0800

    update

--- txt_2_video.py
+++ txt_2_video.py
@@ -3,12 +3,13 @@ from PIL import Image
 import imageio
 import torch
 import numpy as np
-from diffusers import TextToVideoZeroPipeline
+from diffusers import TextToVideoZeroPipeline, TextToVideoSDPipeline
 from diffusers.pipelines.text_to_video_synthesis.pipeline_text_to_video_zero import CrossFrameAttnProcessor
 from diffusers.pipelines.stable_diffusion import safety_checker
 import cv2
 from compel import Compel, ReturnedEmbeddingsType
 import sys
+from diffusers.utils import load_image, export_to_video
 
 def remove_nsfw_check(self, clip_input, images) :
     return images, [False for i in images]
@@ -20,7 +21,7 @@ model_name = sys.argv[1]
 print("using model: " +model_name)
 model_id = "/home/likezhang/models/{}.safetensors".format(model_name)
 
-pipe = TextToVideoZeroPipeline.from_single_file(
+pipe = TextToVideoSDPipeline.from_single_file(
     model_id, 
     torch_dtype=torch.float16, 
     safety_checker=None, 
@@ -35,22 +36,12 @@ prompt = '''
 compel = Compel(tokenizer=pipe.tokenizer, text_encoder=pipe.text_encoder)
 weighted_prompt = compel([prompt])
 
-result = []
-chunk_ids = np.arange(0, video_length, chunk_size - 1)
 generator = torch.Generator(device="cuda")
-for i in range(len(chunk_ids)):
-    print(f"Processing chunk {i + 1} / {len(chunk_ids)}")
-    ch_start = chunk_ids[i]
-    ch_end = video_length if i == len(chunk_ids) - 1 else chunk_ids[i + 1]
-    # Attach the first frame for Cross Frame Attention
-    frame_ids = [0] + list(range(ch_start, ch_end))
-    # Fix the seed for the temporal consistency
-    generator.manual_seed(seed)
-    output = pipe(prompt_embeds=weighted_prompt, pooled_prompt_embeds = None, video_length=len(frame_ids), generator=generator, width=256, height=256, num_inference_steps=100, frame_ids=frame_ids)
-    result.append(output.images[1:])
+output = pipe(
+    prompt_embeds=weighted_prompt, 
+    pooled_prompt_embeds = None, 
+    num_frames=8, 
+    generator=generator, width=256, height=256, num_inference_steps=100).frames
 
-# Concatenate chunks and save
-result = np.concatenate(result)
-result = [(r * 255).astype("uint8") for r in result]
-imageio.mimsave("video.mp4", result, fps=4)
-
+video_path = export_to_video(output)
+path = export_to_video(output, 'video.mp4', fps=7)
