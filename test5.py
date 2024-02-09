8575a12da193e6401d23270b711eeca046dc71ff
likezhang <likezhang@Likes-MacBook-Air.local>
2024-02-09 14:07:19 -0800

    update

--- test5.py
+++ test5.py
@@ -1,15 +1,21 @@
-from diffusers import DiffusionPipeline, UNet2DConditionModel
-from transformers import CLIPTextModel
+from diffusers import AutoPipelineForText2Image
 import torch
 
-unet = UNet2DConditionModel.from_pretrained("/home/likezhang/output/pytorch_lora_weights.safetensors")
+pipeline = AutoPipelineForText2Image.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16).to("mps")
 
-# if you have trained with `--args.train_text_encoder` make sure to also load the text encoder
-# text_encoder = CLIPTextModel.from_pretrained("path/to/model/checkpoint-100/checkpoint-100/text_encoder")
+NEGATIVE_PROMPT="(worst quality, low quality, normal quality:1.4), lowres, bad anatomy, ((bad hands)), text, error, missing fingers, extra digit, fewer digits,head out of frame, cropped, letterboxed, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, censored, letterbox, blurry, monochrome, fused clothes, nail polish, boring, extra legs, fused legs, missing legs, missing arms, extra arms, fused arms, missing limbs, mutated limbs, dead eyes, empty eyes, 2girls, multiple girls, 1boy, 2boys, multiple boys, multiple views, jpeg artifacts, text, signature, watermark, artist name, logo, low res background, low quality background, missing background, white background,deformed"
 
-pipeline = DiffusionPipeline.from_pretrained(
-    "runwayml/stable-diffusion-v1-5", unet=unet, dtype=torch.float16,
-).to("cuda")
+pipeline.load_lora_weights("/Users/likezhang/projects/models/likezhang.safetensors")
+prompt = "likezhang is looking at you. likezhang is 35 years old. {likezhang},front face, portrait, close up, best quality, masterpiece, realistic,masterpiece,vivid,realistic,photorealistic"
+images = pipeline(
+            prompt = prompt,
+            negative_prompt = NEGATIVE_PROMPT,
+            num_images_per_prompt=4,
+            num_inference_steps=100                
+            ).images
 
-image = pipeline("likezhang is walking on street", num_inference_steps=50, guidance_scale=7.5).images[0]
-image.save("likezhang.png")
+i = 0
+for img in images:
+    img.save("test_{}.png".format(i))
+    i+=1
+
